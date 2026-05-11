9b07fa6bbb1ed7bd987aadb12ba7f77d521948c3
Levvixxx <boyko123d@gmail.com>
2026-05-11 16:37:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,55 @@
+#include <iostream>
+#include <windows.h>
 
+using namespace std;
+
+HWND h = NULL;
+string title = "";
+string newTitle = "";
+
+int searchAp(){
+    cout << "Enter the title of the active application:";
+    std::getline(std::cin, title);
+    cout << "Enter a new name for the active application:";
+    std::getline(std::cin, newTitle);
+    h = FindWindowA(NULL,title.c_str());
+    if(h == NULL){
+        cout << "Application not found!";
+        return 1;
+    }
+    if(newTitle == ""){
+        cout << "NewTitle null!";
+        return 1;
+    }
+    return 0;
+}
+
+int startTyping(){
+    while (true)
+    {
+        string result = "";
+        for(int i = 0;i<newTitle.length();i++){
+            if(!IsWindow(h)){
+                return 0;
+            }
+            result += {newTitle[i]};
+            SetWindowTextA(h,result.c_str());
+            Sleep(800);
+        }
+        Sleep(2000);
+        result = "";
+    }
+}
+
+
+int main(){
+    while(true){
+        if(searchAp() == 0){
+            if(startTyping() == 0){
+                return 0;
+            }
+            break;
+        }
+    }
+    return 0;
+}
